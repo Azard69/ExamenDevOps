61eac41d703bac9ec1db90ce77b42c11ccfd9bc1
Michael Van aerschot <michaelvanaerschot@hotmail.com>
2023-01-20 10:58:21 +0100

    feat(adding test of length of validation gamer tag): the test return true if it has 8 or less char

--- devops-exam-main/src/services/__tests__/validations.test.js
+++ devops-exam-main/src/services/__tests__/validations.test.js
@@ -1,4 +1,5 @@
 const { isEmpty } = require("../validations");
+const { validationGamerTag } = require("../validations");
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -18,3 +19,17 @@ describe("validations tests suites - isEmpty", () => {
 });
 
 // TODO: Create tests suite for validation function
+
+describe("validations tests for the gamerTag Length", () => {
+    test("should return true as the gamertag has 5 characters", () => {
+        const gamerTagValue = "micka";
+        expect(validationGamerTag(gamerTagValue)).toBe(true);
+    });
+});
+
+describe("validations tests for the gamerTag Length", () => {
+    test("should return false as the gamertag has 10 characters", () => {
+        const gamerTagValue = "Michaelles";
+        expect(validationGamerTag(gamerTagValue)).toBe(false);
+    });
+});
--- devops-exam-main/src/services/validations.js
+++ devops-exam-main/src/services/validations.js
@@ -1,4 +1,8 @@
 // TODO: Add here the function to validate
+module.exports.validationGamerTag = (gamerTagValue) => {
+    if (gamerTagValue.length > 8) return false;
+    else return true;
+};
 
 const isEmpty = (label) => !label || label.length === 0;
 
